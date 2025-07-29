4652963cd73c84da33e72fc71d8947f4eb5b4d09
devTalib <developer@devexcelit.com>
2025-07-29 18:19:44 +0500

    minor update

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,3 @@ import app from "./app.js"
 import serverless from "serverless-http";
 
 export const handler = serverless(app);
-
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
