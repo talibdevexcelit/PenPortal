04a8841b23e0c3876b0ce1fa5e202bb2970f5a96
devTalib <developer@devexcelit.com>
2025-07-30 18:50:53 +0500

    testing

--- backend/app.js
+++ backend/app.js
@@ -1,45 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./config/db.js";
-import postRoute from "./routes/postRoute.js";
-import userRoute from "./routes/userRoute.js";
-import adminRoute from "./routes/adminRoute.js";
-import errorHandler from "./middlewares/errorMiddleware.js";
-
+import express from 'express';
 const app = express();
-dotenv.config();
 
-connectDB();
-
-// Middleware
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      "http://localhost:5173",
-      "https://penportal.vercel.app", // Removed extra space
-    ];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => res.send("Hello, World!"));
-
-// app.js
-app.get("/favicon.ico", (req, res) => res.status(204).end());
-
-app.use("/api/blog", postRoute);
-app.use("/api/auth", userRoute);
-app.use("/api/admin", adminRoute);
+app.get('/', (req, res) => {
+  res.send('Test response from Express on Vercel');
+});
 
 export default app;
