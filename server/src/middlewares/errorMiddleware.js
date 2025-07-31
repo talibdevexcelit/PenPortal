228901e09307c82e3eb331c3ba5556d1dfa3bded
devTalib <developer@devexcelit.com>
2025-07-31 11:31:33 +0500

    updates

--- server/src/middlewares/errorMiddleware.js
+++ server/src/middlewares/errorMiddleware.js
@@ -1,12 +1,5 @@
 const errorHandler = (err, req, res, next) => {
     console.error(err.stack);
-    
-    // Ensure CORS headers are preserved
-    res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    
     res.status(500).json({
         message: 'Something went wrong!',
         error: process.env.NODE_ENV === 'development' ? err.message : {}
